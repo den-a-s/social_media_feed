110e46db6a19d703b1acbdf0cccb34a6a52fc454
Dennis <pr13RACK@yandex.ru>
2024-12-22 18:02:32 +0300

    Сделал минимальный сервис Добавил html страничку Инициализировал конфиг и логгер

---
+++ feed/cmd/feed/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+	"social.media.feed/internal/config"
+	mwLogger "social.media.feed/internal/http/middleware/logger"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
+
+	logger := setupLogger(cfg.Env)
+	
+	logger.Info("Start feed app")
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	// r.Use(middleware.RealIP)
+	router.Use(middleware.Logger)
+	router.Use(mwLogger.New(logger))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.Timeout(cfg.Timeout))
+
+	router.Get("/resources/*", func(w http.ResponseWriter, r *http.Request) {
+		fs := http.StripPrefix("/resources/", http.FileServer(http.Dir("./resources")))
+		fs.ServeHTTP(w, r)
+	})
+  
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		fs := http.StripPrefix("/", http.FileServer(http.Dir("./resources")))
+		fs.ServeHTTP(w, r)
+	})
+
+	address := fmt.Sprintf("%s:%s", cfg.HTTPServer.Host, cfg.HTTPServer.Port)
+
+	http.ListenAndServe(address, router)
+}
+
+func setupLogger(env string) *slog.Logger {
+	curr_time := time.Now()
+	err := createFolderIfNotExists("logs")
+	if err != nil {
+		panic(err)
+	}
+	f, err := os.Create(fmt.Sprintf("./logs/feed_%s.txt", curr_time.Format(time.RFC3339)))
+	if err != nil {
+		panic(err)
+	}
+	
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(f, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewTextHandler(f, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewTextHandler(f, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default: // If env config is invalid, set prod settings by default due to security
+		log = slog.New(
+			slog.NewTextHandler(f, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func createFolderIfNotExists(path string) error {
+	err := os.MkdirAll(path, 0750)
+	if os.IsExist(err) {
+		return nil
+	}
+    return err
+}
---
+++ feed/internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env string `yaml:"env" env-required:"true"`
+	HTTPServer `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Host string `yaml:"host" env-required:"true"`
+	Port string `yaml:"port" env-required:"true"`
+	Timeout time.Duration `yaml:"timeout" env-required:"true"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-required:"true"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		panic("CONFIG_PATH is not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic(fmt.Sprintf("Config is not exist on path: %s", configPath))
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		panic(fmt.Sprintf("Cannot read config: %s", err))
+	}
+
+	return &cfg
+}
