e0d287efd01182b6a06729348d4244a172ef90af
Dennis <pr13RACK@yandex.ru>
2025-01-08 14:46:30 +0300

    Мелкие огрехи

--- sso/migrations/1_init.up.sql
+++ sso/migrations/1_init.up.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS Users
 
 INSERT INTO Users(email, pass_hash, is_admin)
 VALUES(
-    "matfak@uniyar.ac.ru", 
+    "matfak@uniyar.ac.ru",
     "$2a$10$mN21yv6wSRu91xfl0/s6kuwBT77LnI0Wv9H18Ka.3qaXCMNld9c.e",
     True
 )
